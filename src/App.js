f333e3fee1f5366e4b5dd4736199f632e42069f3
Brian <bayuan@purdue.edu>
2023-09-08 21:42:50 -0400

    rm test text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>TEST TEST TESTOpen up App.js to start working on your app!</Text>
+      <Text>We <3 ECE 477!</Text>
       <StatusBar style="auto" />
     </View>
   );

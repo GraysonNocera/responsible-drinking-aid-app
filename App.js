4284eb8c079d94a7fe6f2dc71e53237e9719b5a4
Grayson Nocera <gnocera@purdue.edu>
2023-09-12 21:33:09 -0400

    feat: added navigation (will refactor later)

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>BAC: 0</Text>
+      <Text>Heartrate: 100</Text>
+      <Button title="Settings"
+        onPress={() => navigation.navigate('Settings')}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Settings</Text>
+      <TextInput inputMode="text" placeholder="Height"/>
+      <Button title="Home"
+        onPress={() => navigation.navigate('Home')}/>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 

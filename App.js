1509a357bdf7574540612c6ac78d8cf85e529484
Grayson Nocera <gnocera@purdue.edu>
2023-10-20 11:46:45 -0400

    setup notifications at app launch

--- App.js
+++ App.js
@@ -3,16 +3,19 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import 'expo-dev-client';
 import { RealmProvider } from '@realm/react';
 import React from 'react';
-// import bluetoothReceiver from './src/services/BluetoothReceiver';
 import Home from './src/pages/Home';
 import Settings from './src/pages/Settings';
 import Emergency from './src/pages/Emergency';
+import { setupNotifications } from './src/services/notifications';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   // TODO - ask user for location permissions (otherwise bluetooth fails)
   // TODO - bluetooth here or on home screen?
+
+  // Setup notifications
+  setupNotifications();
 
   return (
     <RealmProvider>
